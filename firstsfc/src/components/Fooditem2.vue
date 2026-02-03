75c2298402cacf33d6c6ff78679f451913df3936
John Carlo E Baracena <jebaracena@student.apc.edu.ph>
2026-02-03 18:26:51 +0800

    Update Fooditem2.vue

--- firstsfc/src/components/Fooditem2.vue
+++ firstsfc/src/components/Fooditem2.vue
@@ -1,1 +1,27 @@
+<template>
+  <div class="durian">
+    <h2>{{ name }}</h2>
+    <p>{{ message }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: 'Durian',
+      message: 'I like durian'
+    }
+  }
+};
+</script>
+
+<style>
+  .durian {
+    border: dashed black 1px;
+    display: inline-block;
+    margin: 10px;
+    padding: 10px;
+    background-color: rgb(213, 237, 76);
+  }
 
