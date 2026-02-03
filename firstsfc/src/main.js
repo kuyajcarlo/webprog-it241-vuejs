ca655430354db5c3882d20365326c9aefc04b137
John Carlo E Baracena <jebaracena@student.apc.edu.ph>
2026-02-03 18:52:23 +0800

    Update main.js

--- firstsfc/src/main.js
+++ firstsfc/src/main.js
@@ -1,4 +1,12 @@
 import { createApp } from 'vue'
-import App from './App.vue'
 
-createApp(App).mount('#app')
+import App from './App.vue'
+import Fooditem from './components/Fooditem.vue'
+import Fooditem2 from './components/Fooditem2.vue'
+import Profile from './components/Profile.vue'
+
+const app = createApp(App)
+app.component('food-item', Fooditem)
+app.component('food-item2', Fooditem2)
+app.component('profile', Profile)
+app.mount('#app')
