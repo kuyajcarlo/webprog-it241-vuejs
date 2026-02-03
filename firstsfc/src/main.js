280a08ca7ddd9de4dd14eaf7599bc7190ec0c8ec
John Carlo E Baracena <jebaracena@student.apc.edu.ph>
2026-02-03 18:29:08 +0800

    Fix import case for FoodItem components

--- firstsfc/src/main.js
+++ firstsfc/src/main.js
@@ -1,6 +1,6 @@
 import App from './App.vue'
-import FoodItem from './components/FoodItem.vue'
-import FoodItem2 from './components/FoodItem2.vue'
+import FoodItem from './components/Fooditem.vue'
+import FoodItem2 from './components/Fooditem2.vue'
 
 const app = createApp(App)
 app.component('food-item', FoodItem)
