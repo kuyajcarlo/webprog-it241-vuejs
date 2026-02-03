776b7102b746f5ed8e44545f630aed33f02d0823
John Carlo E Baracena <jebaracena@student.apc.edu.ph>
2026-02-03 09:24:37 +0000

    sample

---
+++ firstsfc/src/components/Fooditem.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="santol">
+    <h2>{{ name }}</h2>
+    <p>{{ message }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: 'Santol and Mango',
+      message: 'I like santol and mango'
+    }
+  }
+};
+</script>
+
+<style>
+  .santol {
+    border: dashed black 1px;
+    display: inline-block;
+    margin: 10px;
+    padding: 10px;
+    background-color: brown;
+  }
+
+</style>
