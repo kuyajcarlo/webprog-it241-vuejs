906a765570f43893b63229b1b244254171a4cd70
John Carlo E Baracena <jebaracena@student.apc.edu.ph>
2026-02-03 18:45:59 +0800

    Update main.js

--- firstsfc/src/main.js
+++ firstsfc/src/main.js
@@ -8,4 +8,5 @@ import Profile from './components/Profile.vue'
 const app = createApp(App)
 app.component('food-item', Fooditem)
 app.component('food-item2', Fooditem2)
+app.component('profile', Profile)
 app.mount('#app')
