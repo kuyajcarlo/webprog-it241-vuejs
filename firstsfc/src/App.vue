dfee688fae51adf915265aa770b3d10f8928c9b5
John Carlo E Baracena <jebaracena@student.apc.edu.ph>
2026-02-03 18:52:38 +0800

    Update App.vue

--- firstsfc/src/App.vue
+++ firstsfc/src/App.vue
@@ -1,24 +1,11 @@
 <template>
-  <div>
-    <h1>Food</h1>
-    <Profile />
-    <FoodItem2 />
-    <FoodItem />
-  </div>
+  <h1>Food</h1>
+  <profile/> 
+  <food-item2/>
+  <food-item/>
+
 </template>
 
-<script>
-import FoodItem from './components/Fooditem.vue'
-import FoodItem2 from './components/Fooditem2.vue'
-import Profile from './components/Profile.vue'
-
-export default {
-  components: {
-    FoodItem,
-    FoodItem2,
-    Profile
-  }
-}
-</script>
+<script></script>
 
 <style></style>
