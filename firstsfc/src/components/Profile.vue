d3171f543f08eb26a7637883113f1cad050f4720
John Carlo E Baracena <jebaracena@student.apc.edu.ph>
2026-02-03 18:46:52 +0800

    Update Profile.vue

--- firstsfc/src/components/Profile.vue
+++ firstsfc/src/components/Profile.vue
@@ -1,41 +1,60 @@
-<!DOCTYPE HTML>
-<html>
-<head>
-    <title>Personal Profile</title>
-    <link rel="stylesheet" href="css/mystyle.css">
-</head>
+<template>
+  <div>
+    <h1>Personal Profile Web Page</h1>
 
-<body>
-
-<h1>Personal Profile Web Page</h1>
-
-<div class="container">
-
-    <div class="about-section">
+    <div class="container">
+      <div class="about-section">
         <h2>About Me</h2>
 
         <p>
-            Course: BSIT (Bachelor of Science in Information Technology)<br>
-            IT Experience: JavaScript, Python Programming, SQL<br>
-            Interests: Gaming, Badminton, Cycling<br>
-            Goal: Successful Entrepreneur
+          Course: BSIT (Bachelor of Science in Information Technology)<br>
+          IT Experience: JavaScript, Python Programming, SQL<br>
+          Interests: Gaming, Badminton, Cycling<br>
+          Goal: Successful Entrepreneur
         </p>
 
-        <img src="Introduction/JPGimage/DSC00501.jpg" 
-             alt="John Carlo Baracena" 
-             class="profile-pic">
-    </div>
+        <img
+          src="@/assets/DSC00501.jpg"
+          alt="John Carlo Baracena"
+          class="profile-pic"
+        />
+      </div>
 
-    <div class="gallery">
+      <div class="gallery">
         <h2>My Gallery</h2>
 
-        <iframe 
-            src="https://assets.pinterest.com/ext/embed.html?id=2251868558892663" 
-            height="550" width="450">
+        <iframe
+          src="https://assets.pinterest.com/ext/embed.html?id=2251868558892663"
+          height="550"
+          width="450">
         </iframe>
+      </div>
     </div>
+  </div>
+</template>
 
-</div>
+<script>
+export default {
+  name: "Profile"
+};
+</script>
 
-</body>
-</html>
+<style scoped>
+/* Move or rewrite your mystyle.css styles here */
+
+.container {
+  display: flex;
+  gap: 20px;
+}
+
+.about-section,
+.gallery {
+  border: 1px solid #ccc;
+  padding: 15px;
+}
+
+.profile-pic {
+  width: 200px;
+  margin-top: 10px;
+}
+</style>
