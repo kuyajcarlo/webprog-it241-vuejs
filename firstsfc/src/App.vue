2438f5a468d27e8570b2aad70dc9a88f30db33d4
John Carlo E Baracena <jebaracena@student.apc.edu.ph>
2026-02-07 02:12:09 +0800

    Update App.vue

--- firstsfc/src/App.vue
+++ firstsfc/src/App.vue
@@ -5,24 +5,46 @@ import { supabase } from './lib/supabaseClient'
 const instruments = ref([])
 const comments = ref([])
 
-
 async function getInstruments() {
-  const { data } = await supabase.from('instruments').select()
+  const { data, error } = await supabase
+    .from('instruments')
+    .select()
+
+  if (error) {
+    console.error(error)
+    return
+  }
+
   instruments.value = data
-  async function getComments() {
-  const { data } = await supabase.from('comments').select()
+}
+
+async function getComments() {
+  const { data, error } = await supabase
+    .from('comments')
+    .select()
+
+  if (error) {
+    console.error(error)
+    return
+  }
+
   comments.value = data
 }
-  onMounted(() => {
-   getInstruments()
-   getComments()
+
+onMounted(() => {
+  getInstruments()
+  getComments()
 })
 </script>
 
 <template>
   <ul>
-    <li v-for="instrument in instruments" :key="instrument.id">{{ instrument.name }}</li>
-    <li v-for="comment in comments" :key="comment.id">{{ comment.name }}</li>
+    <li v-for="instrument in instruments" :key="instrument.id">
+      {{ instrument.name }}
+    </li>
 
+    <li v-for="comment in comments" :key="comment.id">
+      {{ comment.name }}
+    </li>
   </ul>
 </template>
