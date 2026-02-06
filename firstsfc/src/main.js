c0e1bdff3c60a92d29f5eeaa8181354db3ed0d56
John Carlo E Baracena <jebaracena@student.apc.edu.ph>
2026-02-07 03:57:28 +0800

    Update component imports and registrations in main.js

--- firstsfc/src/main.js
+++ firstsfc/src/main.js
@@ -1,17 +1,18 @@
 import { createApp } from 'vue'
-
 import App from './App.vue'
+
 import PersonalProfile from './components/Profile.vue'
 import Fooditem from './components/Fooditem.vue'
 import Fooditem2 from './components/Fooditem2.vue'
 import CommentForm from './components/CommentForm.vue'
-import Comment from './components/comment.vue'
-
+import Comments from './components/Comments.vue'
 
 const app = createApp(App)
-app.component('personal-profile', Profile)
+
+app.component('personal-profile', PersonalProfile)
 app.component('food-item', Fooditem)
 app.component('food-item2', Fooditem2)
 app.component('comment-form', CommentForm)
-app.component('comment', comment)
+app.component('comments', Comments)
+
 app.mount('#app')
