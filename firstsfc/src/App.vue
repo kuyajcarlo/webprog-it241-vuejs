40a191fd9fed4bd351760d9e38ab3b593b1d9e44
John Carlo E Baracena <jebaracena@student.apc.edu.ph>
2026-02-07 02:10:38 +0800

    Update App.vue

--- firstsfc/src/App.vue
+++ firstsfc/src/App.vue
@@ -1,11 +1,28 @@
+<script setup>
+import { ref, onMounted } from 'vue'
+import { supabase } from './lib/supabaseClient'
+
+const instruments = ref([])
+const comments = ref([])
+
+
+async function getInstruments() {
+  const { data } = await supabase.from('instruments').select()
+  instruments.value = data
+  async function getComments() {
+  const { data } = await supabase.from('comments').select()
+  comments.value = data
+}
+  onMounted(() => {
+   getInstruments()
+   getComments()
+})
+</script>
+
 <template>
-  <h1>Food</h1>
-  <food-item/>
-  <personal-profile/>
-  <comment-form/>
-  <comment/>
-   
+  <ul>
+    <li v-for="instrument in instruments" :key="instrument.id">{{ instrument.name }}</li>
+    <li v-for="comment in comments" :key="comment.id">{{ comment.name }}</li>
 
+  </ul>
 </template>
-
-<script></script>
